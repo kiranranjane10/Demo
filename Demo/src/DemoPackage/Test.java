6fb3f47f98e860293e132f6fbcd0e74f13f3374a
kiranranjane10 <143025128+kiranranjane10@users.noreply.github.com>
2023-08-23 23:47:27 +0530

    Update Test.java

--- Demo/src/DemoPackage/Test.java
+++ Demo/src/DemoPackage/Test.java
@@ -2,8 +2,14 @@ package DemoPackage;
 
 public class Test 
 {
+	public void m1()
+	{
+		System.out.println("m1--Test");
+	}
 	public static void main(String[] args) 
 	{
+		Test t=new Test();
+		t.m1();
 		System.out.println("Demo Program");
 
 	}
